2c7352b0c151d68ef9df02d0880c10cce1c6099c
MarkZsombor <markszsombor@gmail.com>
2017-11-14 21:49:43 +0000

    cleaned up code and removed console.logs

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,8 @@ $(document).ready(function() {
   console.log("Loaded from the docready callback");
 
   $( ".new-tweet textarea" ).keyup(function() {
-    // console.log( "Handler for .keyup() called." );
     var counterValue = $(this).val();
     var remainingChars = 140 - counterValue.length;
-    // console.log(remainingChars);
     $(this).parent().children('.counter').text(function() {
       return remainingChars;
     });
