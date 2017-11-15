e5196985b72c07230800a7aba8dffbf34332331d
MarkZsombor <markszsombor@gmail.com>
2017-11-15 20:09:30 +0000

    used jquery to pull information from within the text box

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -51,44 +51,69 @@ var data = [
   }
 ];
 
-function createTweetElement(tweetObject) {
 
-  var $header = $('<header>')
-      .append(`<img class="avatar" src="${tweetObject.user.avatars.regular}">`)
-      .append(`<h2 class="user-name">${tweetObject.user.name}</h2>`)
-      .append(`<p class="handle">${tweetObject.user.handle}</p>`);
 
-  var $section = $('<section>')
-      // .append(`<p class="tweet-content">${tweetObject.content.text}</p>`);
+$(document).ready(function() {
+
+  function createTweetElement(tweetObject) {
+
+    var $header = $('<header>')
+        .append(`<img class="avatar" src="${tweetObject.user.avatars.regular}">`)
+        .append(`<h2 class="user-name">${tweetObject.user.name}</h2>`)
+        .append(`<p class="handle">${tweetObject.user.handle}</p>`);
+
+    var $section = $('<section>')
+        // .append(`<p class="tweet-content">${tweetObject.content.text}</p>`);
       .append('<p>').text(tweetObject.content.text).addClass('tweet-content');
 
-  var creationDate = convertDate(tweetObject.created_at);
+    var creationDate = convertDate(tweetObject.created_at);
 
-  var $footer = $('<footer>')
+    var $footer = $('<footer>')
       .append(`<p class="date">${creationDate}</p>`)
       .append('<i class="fa fa-heart">')
       .append('<i class="fa fa-retweet">')
       .append('<i class="fa fa-flag">');
 
-  var $newTweet = $('<article class="tweet">')
+    var $newTweet = $('<article class="tweet">')
       .append($header, $section, $footer);
 
 
-  return $newTweet;
-}
-
-function renderTweets(tweets) {
-  for (var entry of tweets) {
-    var newTweet = createTweetElement(entry);
-    $('#tweets-container').append(newTweet);
+    return $newTweet;
   }
-}
+
+  function renderTweets(tweets) {
+    for (var entry of tweets) {
+      var newTweet = createTweetElement(entry);
+      $('#tweets-container').append(newTweet);
+    }
+  }
 
 
-function convertDate(timeCode) {
-  return new Date(timeCode).toUTCString();
-}
+  function convertDate(timeCode) {
+    return new Date(timeCode).toUTCString();
+  }
 
-$(document).ready(function() {
+  function getNewTweet() {
+    var $button = $('#new-tweet');
+    $button.on('click', function() {
+      event.preventDefault();
+      console.log('the button is clicked');
+      // console.log($(this)); // form#new-tweet
+      var $form = $(this);
+      // console.log($form.children('textarea').val());
+      console.log($form.serialize());
+      // $.ajax({
+      //   type: 'POST',
+      //   url:  '/',
+      //   data: $form.serialize()
+      // })
+      //   .done(function() {
+      //     var newTweet = $form.children('textarea').val();
+
+      //   });
+    });
+  }
+
   renderTweets(data);
+  getNewTweet();
 });
