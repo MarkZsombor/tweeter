e6d940c150127f8e4d82a3be2bd554c0515bcf51
MarkZsombor <markszsombor@gmail.com>
2017-11-16 23:59:19 +0000

    cleaned up code, added comments

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,6 +7,12 @@
 
 $(document).ready(function() {
 
+/*
+ * uses jQuery to create a new tweet item to be rendered in the DOM
+ *
+ * @param {object} tweetObject - information taken from the database for a tweet
+ * @return {string} all the information needed for jQuery to render a tweet in the DOM
+ */
   function createTweetElement(tweetObject) {
 
     var $header = $('<header>')
@@ -15,7 +21,6 @@ $(document).ready(function() {
         .append(`<p class="handle">${tweetObject.user.handle}</p>`);
 
     var $content = $('<div>')
-        // .append(`<p class="tweet-content">${tweetObject.content.text}</p>`);
       .append('<p>').text(tweetObject.content.text).addClass('tweet-content');
 
     var creationDate = convertDate(tweetObject.created_at);
@@ -33,6 +38,11 @@ $(document).ready(function() {
     return $newTweet;
   }
 
+/*
+ * creates a new element in the DOM
+ *
+ * @param {string} tweets - jQuery data for the new item
+ */
   function renderTweets(tweets) {
     $('#tweets-container').empty();
     for (var entry of tweets) {
@@ -41,19 +51,28 @@ $(document).ready(function() {
     }
   }
 
-
+/*
+ * converts timecode to calendar date + time
+ *
+ * @param {number} timeCode - a time code
+ * @return {string} date as a UTC string
+ */
   function convertDate(timeCode) {
     return new Date(timeCode).toUTCString();
   }
 
+/*
+ * creates a new tweet when Submit button is clicked and renders it on the page
+ *
+ */
   function getNewTweet() {
     var $button = $('#new-tweet');
     $button.on('click', function() {
       event.preventDefault();
-      console.log('the button is clicked');
-      // console.log($(this)); // input#new-tweet
+      // console.log($(this)); = input#new-tweet
       var $form = $(this).parent();
       // an empty tweet will have $form.serialize().length of 5
+      // need error msgs if tweet is empty or too long
       if ($form.serialize().length <= 5) {
         alert("Tweets can't be blank");
         return;
@@ -61,6 +80,7 @@ $(document).ready(function() {
         alert("Tweets can't be more than 140 characters");
         return;
       }
+      // POST the text content to the server, when done load the tweet, clear the textarea and reset the char counter
       $.ajax({
         type: 'POST',
         url:  '/tweets/',
@@ -74,6 +94,10 @@ $(document).ready(function() {
     });
   }
 
+/*
+ * gets all tweets from the database and renders them on the page
+ *
+ */
   function loadTweets() {
     $.ajax({
       type: "GET",
@@ -85,15 +109,18 @@ $(document).ready(function() {
     });
   }
 
+  // populate the page with tweets in database
   loadTweets();
   getNewTweet();
 
+  // clicking the compose button will toggle the new tweet area and focus on the textarea
   $( ".compose" ).click(function() {
     $( ".new-tweet" ).slideToggle( "slow", function() {
     $('textarea').focus().select();
     });
   });
 
+  // textarea will be focused on page load
   $(function() {
     $('textarea').focus().select();
   });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   console.log("Loaded from the docready callback");
 
+
+// This is alter the text on displayed character counter to show 140-used characters, the text is displayed in black for character counts between 0-140, and will be red if the count is greater than 140
   $( ".new-tweet textarea" ).keyup(function() {
     var counterValue = $(this).val();
     var remainingChars = 140 - counterValue.length;
