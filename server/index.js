ba1c14e61528e42e6edd0147a97ff72214de105a
MarkZsombor <markszsombor@gmail.com>
2017-11-17 19:57:26 +0000

    added node-sass-middleware to server and package.json

--- server/index.js
+++ server/index.js
@@ -2,16 +2,24 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
-const MongoClient   = require("mongodb").MongoClient;
-const MONGODB_URI   = "mongodb://localhost:27017/tweeter";
+const PORT            = 8080;
+const express         = require("express");
+const bodyParser      = require("body-parser");
+const app             = express();
+const MongoClient     = require("mongodb").MongoClient;
+const MONGODB_URI     = "mongodb://localhost:27017/tweeter";
+const sassMiddleware  = require("node-sass-middleware");
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+// should compile sass when page loads
+app.use(sassMiddleware({
+  src: "./public/",
+  debug: true,
+  outputStyle: 'expanded'
+}));
 
 MongoClient.connect(MONGODB_URI, (err, db) => {
   if (err) {
@@ -30,6 +38,7 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
   // so it can define routes that use it to interact with the data layer.
   const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+
 
   // Mount the tweets routes at the "/tweets" path prefix:
   app.use("/tweets", tweetsRoutes);
